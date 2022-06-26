fae6a9a0c0cfbbd4e9569cb8c7b7ab011040ef84
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-25 23:17:30 -0400

    Add generatePassword function variables for parameters

--- script.js
+++ script.js
@@ -15,6 +15,36 @@ const randomFunc = {
     number: getRandomNumber,
     symbol: getRandomsymbol
 };
+//Generate event listen
+generateEl.addEventListener('click', () => {
+    //Unary operator turns string into number//
+    const length = +lengthEl.value;
+    //.checked indicates the current "checked" state of checkboxes, radio buttons, and other widgets that have a checked state.//
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+//Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+    //1. Initialize password variables
+    //2. Filter out unchecked types
+    //3. Loop over length and call generator function for each type
+    //4. Add final password to the password var and return
+
+    let generatePassword = '';
+
+    const typesCount = lower + upper + number + symbol;
+
+    console.log('typesCount: ', typesCount);
+
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+    console.log('typesArr: ', typesArr);
+
+}
 
 // Generator functions
 
@@ -34,6 +64,5 @@ function getRandomNumber() {
 function getRandomsymbol() {
     const symbols = '!@#$%^&*()_+{}[]/;';
     return symbols[Math.floor(Math.random() * symbols.length)];
-    }
+}
 
-console.log(getRandomsymbol());
